fe0e4164218d190f69b52e5c60833eeff2f116c8
JDKanius <kanustu@gmail.com>
2025-08-06 09:45:29 -0600

    2nd commit

---
+++ day_2.rs
@@ -0,0 +1,16 @@
+// vector mainpulation and boolean checks
+fn main () {
+    let mut var = vec![1,3,5,7];
+    let result = vec_prac(var.clone()); // using clone to avoid ownership issues
+    println!("{}", result);
+    var.push(15);
+    println!("{:?}", var)
+}
+
+fn vec_prac(val: Vec<i32>) -> bool {
+    if val[0] == 1 {
+        true
+    } else {
+        false
+    }
+}
